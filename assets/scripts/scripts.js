0666e2b7c506fc5f3d0cd0bf22f0fd920df40f85
jaycossey <jaycossey@live.com>
2023-11-27 17:02:17 +0000

    added load screen animation, added triggers for name and story beginning

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,20 +1,21 @@
 // GLOBALS -------------------------------------------------------
 
-/**
- * I want this program to be a simple journey using css as the base, a simple 
- * hello world, followed by a series of css art, render simple hello world with 
- * animated text
- * 
- * onclick then change the images shown using js but create images with css
- * 
- * start simple then expand on ideas
- */
-
 // Root div for all content
 const ROOT_DIV = document.getElementById('root');
 
 // Welcome text string
 const hello = "$ Hello, World! Hit return to begin...";
+
+// Username storage:
+let userName;
+
+
+// WELCOME FUNCTIONS --------------------------------------------
+
+// clear screen function
+function clearScreen() {
+    $('#root').empty();
+}
 
 // Welcome user to site
 function welcomeText() {
@@ -29,8 +30,106 @@ function welcomeText() {
             pEl.textContent += hello[i];
         }, 60 * i);
     }
-
+    document.addEventListener('keypress', handleStart);
     return;
 }
 
+// event listener to handle start of program
+function handleStart() {
+    // conditional for return key
+    if (event.key === 'Enter') {
+        // prompt name and remove listener
+        promptName();
+        document.removeEventListener('keypress', handleStart);     
+    }
+}
+
+// event listener for username
+function handleUser() {
+    if(event.key === 'Enter') {
+        userName = event.target.value;
+        clearScreen();
+        loadScreen();
+    }
+}
+
+// Step 1
+function promptName() {
+    // create prompt text el
+    let pEl = document.createElement('p');
+    pEl.className = "welcome"
+
+    // create input el
+    let inputEl = document.createElement('input');
+    inputEl.id = "userName";
+    inputEl.className = "input";
+    inputEl.value = "";
+
+    // default value text
+    let value = "$ Enter your name and hit return: ";
+
+    for (let i = 0; i < value.length; i++) {
+        setTimeout(() => {
+            pEl.textContent += value[i];
+        }, 60 * i);
+    }
+
+    // append element and focus
+    ROOT_DIV.append(pEl, inputEl);
+    inputEl.focus();
+
+    // event listener to handle username
+    inputEl.addEventListener('keypress', handleUser);
+}
+
+
+// STORY START FUNCTIONS --------------------------------------
+
+// initial load screen to begin "story" 
+function loadScreen() {
+    // clear background color
+    ROOT_DIV.style.backgroundColor = "black";
+    
+    // Create div for "load" and append
+    let loadDiv = document.createElement('div');
+    loadDiv.className = "loading";
+    ROOT_DIV.append(loadDiv);
+
+    // create text display
+    let pEl = document.createElement('p');
+    pEl.className = "loadText";
+    pEl.textContent = "Welcome, " + userName;
+    ROOT_DIV.append(pEl);
+
+    // Set Timeout before next call
+    setTimeout(() => {
+        clearScreen();
+        fireStory();
+    }, 5000)
+}
+
+function fireStory() {
+    // This function begins the "story"
+    /**
+     * I need to trigger a css animation with some basic story text
+     * This function needs to create a p element and then fill it with
+     * some generic story thing
+     * This function needs to fade in a "fire animation div"
+     */
+    console.log("Story Trigger");
+
+    // Create P element to store text
+    let pEl = document.createElement('p');
+    pEl.className = "storyText";
+    pEl.innerText = userName + " could smell smoke, they opened their eyes to see a fire.";
+
+    // create div to handle fire "image and animation"
+    let fireDiv = document.createElement('div');
+    fireDiv.className = "fire";
+
+    ROOT_DIV.append(fireDiv, pEl);
+}
+
+
+// FUNCTION CALLS ---------------------------------------------
 welcomeText();
